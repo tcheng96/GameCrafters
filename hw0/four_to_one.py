2a37a6e83df129b74c8d693deb16111991385332
Tom Cheng <tcheng96@berkeley.edu>
2016-09-11 12:15:12 -0700

    Added solver.

--- hw0/four_to_one.py
+++ hw0/four_to_one.py
@@ -20,10 +20,31 @@ def do_moves(move, pos):
 Returns a list of all possible moves that can be made
 """ 
 def gen_moves(pos):
-    all_moves = []
     if pos > 1:
         return [1, 2]
-    elif pos > 0:
+    else
         return [1]
-    else:
-        return all_moves
+
+"""
+Solver for 4-to-1 Game
+"""
+def solver(pos, gameStates):
+    result 
+
+
+
+"""
+Given a position and whose turn it is, determine whether or not the current player wins/loses
+    turn: is it P1's turn
+"""
+def traverse_game_tree(pos):
+    if primitive(pos) != "U":
+        return primitive(pos)
+    curr = "L"
+    all_moves = four.gen_moves(pos)
+    for move in all_moves:
+        after_moves = four.do_moves(move, pos)
+        result = traverse_game_tree(after_moves)
+        if result == four.LOSE: # that means the opponent lost after I made this move
+            curr = four.WIN
+    return curr
